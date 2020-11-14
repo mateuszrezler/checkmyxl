44daf197f0e5ad9b9f2baa01f9546ab6299dd2fc
Mateusz Rezler <mateuszrezler@gmail.com>
2020-11-14 17:59:46 +0100

    Moved utilities to 'src/utils.py' file

--- checkmyxl.py
+++ checkmyxl.py
@@ -1,9 +1,7 @@
-from json import load as load_json
-from os.path import dirname, join as join_path, realpath
-from pandas import read_csv
 from sys import argv
 from src.tasks import TASKS
-from xlwings import Book
+from src.utils import get_abs_path, load_config, load_sample, load_sheet, \
+    skip_header
 
 
 class ColumnChecker(object):
@@ -29,50 +27,16 @@ class ColumnChecker(object):
                     function(row, column, cell, *args)
 
 
-def _load_config():
-    config_path = _get_abs_path('config.json')
-    with open(config_path) as cf:
-        config = load_json(cf)
-    return config
-
-
-def _load_sample():
-    config = _load_config()
-    sample_path = _get_abs_path(config['sample_file'])
-    sample = read_csv(sample_path, header=None).values
-    return sample
-
-
-def _load_sheet():
-    config = _load_config()
-    excel_path = _get_abs_path(config['excel_file'])
-    Book(excel_path).set_mock_caller()
-    book = Book.caller()
-    sheet = book.sheets.active
-    return book, sheet
-
-
-def _get_abs_path(rel_path):
-    parent_dir = dirname(realpath(__file__))
-    return join_path(parent_dir, *rel_path.split('/'))
-
-
-def _skip_header(sheet, selection):
-    return sheet.range(
-        (selection.row+1, selection.column),
-        (selection.last_cell.row, selection.last_cell.column))
-
-
 def main(selection=None):
-    config = _load_config()
-    book, sheet = _load_sheet()
+    config = load_config()
+    book, sheet = load_sheet()
     if selection:
         header = False
         selection = sheet[selection]
     else:
         selection = sheet.used_range
     if config['header']:
-        selection = _skip_header(sheet, selection)
+        selection = skip_header(sheet, selection)
     if config['reset_colors']:
         selection.color = None
     cc = ColumnChecker(sheet, selection)
@@ -80,8 +44,8 @@ def main(selection=None):
 
 
 def make_sample():
-    book, sheet = _load_sheet()
-    sheet['A1'].value = _load_sample()
+    book, sheet = load_sheet()
+    sheet['A1'].value = load_sample()
     sheet.autofit('columns')
 
 
---
+++ src/utils.py
@@ -0,0 +1,39 @@
+from json import load as load_json
+from os.path import dirname, join as join_path, realpath
+from pandas import read_csv
+from xlwings import Book
+
+
+def get_abs_path(rel_path):
+    parent_dir = dirname(realpath(__file__))
+    return join_path(parent_dir, '..', *rel_path.split('/'))
+
+
+def load_config():
+    config_path = get_abs_path('config.json')
+    with open(config_path) as cf:
+        config = load_json(cf)
+    return config
+
+
+def load_sample():
+    config = load_config()
+    sample_path = get_abs_path(config['sample_file'])
+    sample = read_csv(sample_path, header=None).values
+    return sample
+
+
+def load_sheet():
+    config = load_config()
+    excel_path = get_abs_path(config['excel_file'])
+    Book(excel_path).set_mock_caller()
+    book = Book.caller()
+    sheet = book.sheets.active
+    return book, sheet
+
+
+def skip_header(sheet, selection):
+    return sheet.range(
+        (selection.row+1, selection.column),
+        (selection.last_cell.row, selection.last_cell.column))
+
