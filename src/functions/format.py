1c47c301bf309bf5a7b1c6c7ad9954c21614d43e
Mateusz Rezler <mateuszrezler@gmail.com>
2020-11-17 10:55:52 +0100

    Added docstrings to 'format' module

--- src/functions/format.py
+++ src/functions/format.py
@@ -1,8 +1,46 @@
+"""
+Formatting functions for checkmyxl package.
+
+This module provides: functions changing appearance of a sheet.
+
+Functions
+---------
+check    do a logic test and change the background color of a cell.
+group    apply the same background color to cell if the previous one contains
+         the same content.
+
+"""
 group_switch = True
 
 
 def check(cell, logic_test, autocorrect=False, correct_color=(0, 255, 0),
           incorrect_color=(255, 0, 0), corrected_color=(255, 255, 0)):
+    """
+    Do a logic test and change the background color of a cell.
+
+    Parameters
+    ----------
+    cell : xlwings.main.Range
+        Evaluated cell.
+    logic_test : object
+        Logic test.
+    autocorrect : object, optional
+        When considered `True`, cell background color is changed to
+        `corrected_color` and its value is changed to `autocorrect`.
+        When `False`, autocorrection is not applied.
+    correct_color : tuple, optional
+        A tuple of RGB values for cell color when `logic_test is True`.
+        Green by default.
+    incorrect_color : tuple, optional
+        A tuple of RGB values for cell color when `logic_test is False`
+        and `autocorrect is False`.
+        Red by default.
+    corrected_color : tuple, optional
+        A tuple of RGB values for cell color when `logic_test is False`
+        and `autocorrect` is considered `True`.
+        Yellow by default.
+
+    """
     if logic_test:
         cell.color = correct_color
     else:
@@ -14,6 +52,19 @@ def check(cell, logic_test, autocorrect=False, correct_color=(0, 255, 0),
 
 
 def group(cell, colors=[(153, 204, 255), None]):
+    """
+    Apply the same background color to cell if the previous one contains
+    the same content.
+
+    Parameters
+    ----------
+    cell : xlwings.main.Range
+        Evaluated cell.
+    colors : list, optional
+        A pair of colors to apply.
+        Light blue and no background by default.
+
+    """
     global group_switch
     if cell.value == cell.offset(-1, 0).value:
         cell.color = colors[group_switch]
