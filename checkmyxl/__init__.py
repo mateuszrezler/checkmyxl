c94ad12363f62fe578185bb1def3c1dfa808f5e9
Mateusz Rezler <mateuszrezler@gmail.com>
2020-11-25 09:32:32 +0100

    Added docstrings to '__init__.py' script

--- checkmyxl/__init__.py
+++ checkmyxl/__init__.py
@@ -1,3 +1,20 @@
+"""
+checkmyxl
+=========
+
+Automate validation of your data in Microsoft Excel sheet.
+
+This package provides
+  1. Checking user-defined correctness of cells, including autocorrection.
+  2. Showing groups of cells with identical content.
+  3. Any custom operation available in `xlwings` package.
+
+How to use the documentation
+----------------------------
+Documentation is available in the form of docstrings provided with the code.
+Use the built-in `help` function to view a function's docstring.
+
+"""
 from .core import ColumnChecker
 
 
