6be5ee1c53124057ca5d1e69374f1d6ba691fd13
Mateusz Rezler <mateuszrezler@gmail.com>
2020-11-25 09:42:23 +0100

    Added docstrings to 'settings' module

--- config/settings.py
+++ config/settings.py
@@ -1,3 +1,16 @@
+"""
+Startup settings for checkmyxl package.
+
+This module provides: constants for `utils.load_config` function.
+
+Constants
+---------
+EXCEL_FILE      Name of Excel file.
+HEADER          When `True`, checking area background color is removed.
+RESET_COLORS    When `True`, the first row is skipped by `ColumnChecker`.
+SAMPLE_FILE     Name of sample `csv` file.
+
+"""
 EXCEL_FILE = 'book.xlsm'
 HEADER = True
 RESET_COLORS = True
