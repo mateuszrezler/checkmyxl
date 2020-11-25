e732c59f97351aa1d8537d148bf817fee6028d0f
Mateusz Rezler <mateuszrezler@gmail.com>
2020-11-25 09:29:47 +0100

    Added docstrings to 'book.py' script

--- book.py
+++ book.py
@@ -1,3 +1,25 @@
+"""
+Startup script for checkmyxl package.
+
+For proper startup, the file system must be organized in such a way:
+```
+    config - configuration directory
+        custom.py - user-defined functions
+        settings.py - startup settings
+        tasks.py - pipeline of tasks
+    <name>.xlsm - Excel file to be checked
+    <name>.py - this file (name must be the same as the name of the Excel file)
+```
+
+This module provides: functions called directly from the associated Excel file
+containing `xlwings` ribbon and / or vba macros.
+
+Functions
+---------
+main    initialize checkmyxl, open Excel file and do the check.
+undo    reopen the active book without saving it.
+
+"""
 from checkmyxl import ColumnChecker
 from checkmyxl.utils import load_config, load_sheet, run_from_script
 from os.path import dirname, realpath
@@ -6,6 +28,16 @@ from xlwings import Book
 
 
 def main(argv=[], selection=None):
+    """
+    Initialize checkmyxl, open Excel file and do the check.
+
+    argv : list, optional
+        A list of command-line arguments passed to the program.
+        This list is empty when `main` function is called directly from Excel.
+    selection : str, optional
+        Selected range in `<letters><digits>(:<letters><digits)` format.
+
+    """
     if argv:
         run_from_script(argv, excel_path, sample_path)
     sheet = load_sheet()
@@ -14,6 +46,12 @@ def main(argv=[], selection=None):
 
 
 def undo():
+    """
+    Reopen the active book without saving it.
+
+    A workaround solution for inability to undo changes made by scripts.
+
+    """
     book, sheet = load_sheet(book_also=True)
     book.close()
     Book(excel_path)
